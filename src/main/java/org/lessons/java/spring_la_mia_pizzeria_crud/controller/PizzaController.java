6ba246031b8b4158b4a0a3a207d1adc35dc35f83
martinolega <martinolega11@gmail.com>
2026-04-24 23:37:03 +0200

    feat:step-2-3

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -2,12 +2,14 @@ package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -17,9 +19,22 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping("pizzas")
-    public String index(Model model) {
-        List<Pizza> pizzas = pizzaRepository.findAll();
+    public String index(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+        List<Pizza> pizzas;
+        if (keyword != null && !keyword.trim().isEmpty()) {
+            pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword);
+        } else {
+            pizzas = pizzaRepository.findAll();
+        }
         model.addAttribute("pizzas", pizzas);
+        model.addAttribute("keyword", keyword);
         return "pizzas/index";
+    }
+
+    @GetMapping("pizzas/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Pizza pizza = pizzaRepository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "pizzas/show";
     }
 }
--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -2,7 +2,8 @@ package org.lessons.java.spring_la_mia_pizzeria_crud.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
+import java.util.List;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
-    
+    public List<Pizza> findByNameContainingIgnoreCase(String name);
 }
